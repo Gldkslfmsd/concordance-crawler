46ad9f491b2469b0f0b8ace37443a26af683a51f
Dominik Macháček <gldksl@atrey.karlin.mff.cuni.cz>
2015-08-22 18:28:59 +0200

    core modified to return list of links

--- concordance-crawler/core.py
+++ concordance-crawler/core.py
@@ -2,17 +2,19 @@
 
 # This will be a main file of Concordance Crawler. 
 
-import parse
 from multiprocessing import Pool
 import requests
+import datetime
 
 from bazwords import *
+import parse
+
 
 class ConcordanceCrawler():
 # TODO: set bazword generators
 #		bazword_gen = None,
 #		bazwords = None
-	def crawl_concordances(self, target_word, number = 100):
+	def crawl(self, target_word, number = 100):
 		'''Crawls concordances.
 
 		Args:
@@ -20,15 +22,19 @@ class ConcordanceCrawler():
 			number of pages with concordances
 		
 		Returns:
-			TODO
+			list of links, where a link is a dictionary
 		'''
 		bazgen = RandomShortWords()
+		links = []
 		for i in range(number):
 			keyword = bazgen.get_bazword() + " " + target_word
 			bingresult = self.crawlonekeyword(keyword)
-			pageresult = self.visitpage(bingresult)
-			print(pageresult)
+#			pageresult = self.visitpage(bingresult)
+			links.extend(bingresult)
+			
+		return links
 
+	# TODO: make it same
 	def crawl_concurrent(self,bazwords, word, processes=10):
 		"""Same as crawl, but does it concurrently and faster.
 		"""
@@ -36,26 +42,52 @@ class ConcordanceCrawler():
 		bazwords = map(lambda i: i + " " + word, bazwords)
 		result = pool.map(crawlonekeyword, bazwords)
 		return result
-#	for i in result:
-#		print(i)
 
 	def get_keyword_url(self,keyword):
 		replacedspaces = keyword.replace(" ","+")
-		url = "http://www.bing.com/search?q={keyword}&first=1&FORM=PERE1".format(
+		# 59 links on page is maximum, then it blocks (probably)
+		url = "http://www.bing.com/search?q={keyword}&first=1&count=59&FORM=PERE1".format(
 			keyword = replacedspaces
 		)
 		return url
 		
 	def crawlonekeyword(self,keyword):
 		'''Scrapes one keyword.
+
+		Returns:
+			list of links, a link is a dictionary with keys:
+			TODO
 		'''
 		url = self.get_keyword_url(keyword)
 		rawhtml = requests.get(url).text
-		# když je to zablokované:
-		#if rawhtml[15414:15431] == 'Omluvte přerušení':
-		#	pass
 
-		return keyword + "\n" + parse.parseBing(rawhtml)
+		date = ConcordanceCrawler.__date()
+
+		if self.is_blocked(rawhtml):
+			# TODO really just return None?
+			return None
+
+		# adding scraping information to links
+		links = parse.parseBing(rawhtml)
+		for i in links:
+			i['date'] = date
+			i['keyword'] = keyword
+
+		return links
+
+	def is_blocked(self,rawhtml):
+		'''This is quick and easy control of blocked file with captcha
+
+		Returns:
+			True if the query was blocked
+			False otherwise
+		'''
+		# sometimes you get 115 bytes file with a strange code (e.g. if you try 30000
+		# requests per minute)
+		if len(rawhtml)<120:
+			return True 
+		# I can see this in Czech Republic, in other countries it may differ!
+		return 'Omluvte přerušení' in rawhtml
 
 	def visitpage(self,bingresult):
 		'''Visits page and tries to find a concordance'''
@@ -63,6 +95,9 @@ class ConcordanceCrawler():
 		return bingresult
 
 
+	def __date():
+		'''Returns a string with current date.'''
+		return str(datetime.datetime.now())
 
 
 
@@ -72,4 +107,6 @@ class ConcordanceCrawler():
 
 if __name__ == "__main__":
 	cc = ConcordanceCrawler()
-	cc.crawl_concordances("dominik",number = 10)
+	res = cc.crawl("write",number = 10)
+	import pprint
+	print(pprint.pformat(res))

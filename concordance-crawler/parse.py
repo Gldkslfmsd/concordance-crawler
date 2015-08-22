e0bed124661929bbe41894838a53fb1e5c4f29bc
Dominik Macháček <gldksl@atrey.karlin.mff.cuni.cz>
2015-08-22 17:56:53 +0200

    parse returns meaningful results

--- concordance-crawler/parse.py
+++ concordance-crawler/parse.py
@@ -5,25 +5,39 @@ import requests
 
 import bazwords
 
+
 def parseBing(rawhtml):
 	"""Gets and parses Bing SERP.
 
 	Returns:
-		data from page, parsed and prettyprinted  by GoogleScraper parser
-		(it's string, but looks like json)
+		List of dicts, every dict is one link and contains this keys: 
+		link, rank, snippet, title, visible_link
 	"""
-	#rawhtml = requests.get(url).text
-	#f = open("raw_"+bazwords.RandomShortWords().get_bazword()+".html","w")
-	#f.write(rawhtml)
-	#f.close()
-	
-	parser = BingParser(query="")
-	parser .parse(rawhtml)
+
+	parser = BingParser()
+	parser.parse(rawhtml)
 
 	serp = SearchEngineResultsPage()
-	serp.query = ""
 	serp.set_values_from_parser(parser)
-	serp.search_engine_name = "bing"
 
-	parsed_data = str(parser)
+	# this extracts just the list of dicts
+	parsed_data = parser.search_results['results']
+
+	# there is one strange and useless information
+	try: del parsed_data["ads_main"]
+	except: pass
+
 	return parsed_data
+
+if __name__ == "__main__":
+	from core import *
+	cc = ConcordanceCrawler()
+	x = cc.get_keyword_url("dominik")
+	import requests
+	raw = requests.get(x).text
+	open("dom.html","w").write(raw)
+
+	print()
+	import pprint
+	print(pprint.pformat(parseBing(raw)))
+	

cb5cd9c2a5ad28f2ce06165ef2e9be12ef888288
Dominik Macháček <gldksl@atrey.karlin.mff.cuni.cz>
2015-08-23 18:11:11 +0200

    added a function for visiting whole list of links

--- concordance-crawler/visitor.py
+++ concordance-crawler/visitor.py
@@ -5,9 +5,11 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import datetime
 
 
 def get_visible_text(html):
+	#Taken from here: http://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
 	soup = BeautifulSoup(html,"lxml")
 	text = soup.findAll(text=True)
 	def visible(element):
@@ -22,6 +24,16 @@ def get_visible_text(html):
 
 
 def visit(url, target):
+	'''Visits a page on given url and extracts all sentences containing
+	target word from visible text.
+
+	Args:
+		url
+		target
+
+	Returns:
+		a list of sentences
+	'''
 	rawhtml = requests.get(url).text
 	text = get_visible_text(rawhtml)
 
@@ -35,25 +47,60 @@ def visit(url, target):
 	# -> [ "Hello!", " How are you?", "I'm fine." ]
 	sentences = [ x+y for x,y in zip(chunks[::2],chunks[1::2]) ]
 
-	reg = re.compile(".*(\s|^)"+target+"(\s|$).*",flags=re.IGNORECASE)
-
 	# concordances are sentences containing target word as a standalone word
-	concordances = list(filter(reg.match, sentences))
+	regex = re.compile(".*(\s|^)"+target+"(\s|$).*",flags=re.IGNORECASE)
+	concordances = list(filter(regex.match, sentences))
+
+	return concordances
+
+def visit_links(links, target_word):
+	'''Gets list of links, visits them and crawls concordances.
+	
+	Args:
+		links -- list of dicts, where dict is a link and has at least key 'link'
+		target_word
+
+	Returns:
+		list of concordances, where a concordance is a dict with keys:
+			url, date, concordance (a sentence), keyword
+	'''
+	concordances = []
+	for i in links:
+		url = i['link']
+		concs = visit(url,target_word)
+		date = str(datetime.datetime.now())
+		for j in concs:
+			c = {
+				'url':url,
+				'date':date,
+				'concordance':j,
+				'keyword':target_word
+			}
+			concordances.append(c)
+
+#	import pprint
+#	print(pprint.pformat(concordances))
+
 	return concordances
 
 
-if __name__ == "__main__":
-	pairs = [("http://leapfroggroup.org/","group"),
-		("http://www.thinkbabynames.com/meaning/1/Dominik","Dominik"),
-		("https://hungryhouse.co.uk/indian-takeaway","takeaway"),
-		("http://www.writeawriting.com/","write")]
 
-	for url,target in pairs:
-		print()
-		print()
-		print(url,target)
-		print("==========================")
-		con = visit(url,target)
-		for i in con:
-			# to better see ends of lines
-			print(">>>",i,"<<<")
+
+
+if __name__ == "__main__":
+#	pairs = [("http://leapfroggroup.org/","group"),
+#		("http://www.thinkbabynames.com/meaning/1/Dominik","Dominik"),
+#		("https://hungryhouse.co.uk/indian-takeaway","takeaway"),
+#		("http://www.writeawriting.com/","write")]
+
+#	for url,target in pairs:
+#		print()
+#		print()
+#		print(url,target)
+#		print("==========================")
+#		con = visit(url,target)
+#		for i in con:
+#			# to better see ends of lines
+#			print(">>>"+i+"<<<")
+
+	visit_links([{'link':'http://www.writeawriting.com/'}],"write")

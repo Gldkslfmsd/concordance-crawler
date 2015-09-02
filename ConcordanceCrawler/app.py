033fab39f1b0afef85709d2b1134a8f0aaa8b2b8
Dominik Macháček <gldksl@atrey.karlin.mff.cuni.cz>
2015-09-02 14:51:27 +0200

    skeleton of logging, exceptions handling

--- ConcordanceCrawler/app.py
+++ ConcordanceCrawler/app.py
@@ -4,10 +4,17 @@
 
 import argparse
 from sys import stdout
+from json import dumps
+from dict2xml import dict2xml
+import logging
 
 from ConcordanceCrawler import *
 
 def get_args():
+	'''Sets command line arguments
+
+	Returns: a dict with arguments and their values
+	'''
 	parser = argparse.ArgumentParser(description='Crawl concordances')
 
 	parser.add_argument("word",
@@ -19,6 +26,12 @@ def get_args():
 		default=10,
 		type=int,
 		help="number of concordances to be crawled (default %(default)s)"
+		)
+
+	parser.add_argument("-p",
+		default=None,
+		type=int,
+		help="maximum number of concordances per page (default: unlimited)"
 		)
 
 	parser.add_argument("-o","--output",
@@ -34,7 +47,7 @@ def get_args():
 		help="""type of bazword generator, you can choose RANDOM (random
 		four-letter words), WIKI_ARTICLES (words from random articles from
 		English Wikipedia), WIKI_TITLES (words from titles of random articles
-		from English Wikipedia), NUMBERS (increasing numbers from 1)"""
+		from English Wikipedia), NUMBERS (increasing numbers from 0)"""
 		)
 
 	parser.add_argument("-f","--format",
@@ -47,12 +60,42 @@ def get_args():
 	args = vars(parser.parse_args())
 	return args
 
+
+class OutputFormater():
+	def __init__(self, format, output_stream):
+		self.output = output_stream
+		if format=="json":
+			self.to_output = self._output_as_json
+		else:
+			self.to_output = self._output_as_xml
+
+	def _output_as_xml(self,concordance):
+		'''prints one concordance to output'''
+		result = dict2xml({'item':concordance},indent=" "*4,wrap="root") + "\n"
+		self.output.write(result)
+
+	def _output_as_json(self,concordance):
+		result = dumps(concordance,indent=4)+"\n"
+		self.output.write(result)
+
+	def __del__(self):
+		try:
+			self.output.close()
+		except ValueError: 
+			pass
+		
+
 def main():
 	args = get_args()
 	word = args["word"]
 	number = args["n"]
-	output = args["output"]
+	output 	= OutputFormater(
+		format=args["format"],
+		output_stream=args["output"]
+		).to_output
 	baz = args["bazword_generator"]
+	max_per_page = args["p"]
+	page_limited = True if max_per_page else False
 	if baz=="RANDOM":
 		bazgen = RandomShortWords()
 	elif baz=="WIKI_ARTICLES":
@@ -63,30 +106,33 @@ def main():
 		bazgen = IncreasingNumbers()
 
 	def yield_concordances(word):
+		'''Generator crawling concordances'''
 		while True:
 			links = crawl_links(word,1,bazgen)
+			print("page with links crawled")
 			for l in links:
-				concordances = visit_links([l],word)
-				for c in concordances:
+#				l['link']='https://www.diffchecker.com/'
+				#print("link:",l['link'])
+				try:
+					concordances = visit_links([l],word)
+				except requests.exceptions.RequestException as e:
+					print("some error occured",e)
+					continue
+				print("page visited, n concordances found")
+				for i,c in enumerate(concordances):
+					if page_limited and i>max_per_page:
+						break
 					res = { "bing_link": l, "concordance": c }
 					yield res
 
 # get exact number of concordances
-	concordances = [ c for _,c in zip(range(number), yield_concordances(word)) ]
+	concordances = ( c for _,c in zip(range(number), yield_concordances(word)) )
 
-	def output_as_xml():
-		import dict2xml
-		result = dict2xml.dict2xml({'item':concordances},indent=" "*4,wrap="root") + "\n"
-		output.write(result)
+	try:
+		for c in concordances:
+			output(c)
+	except KeyboardInterrupt:
+		print("aborted")
 
-	def output_as_json():
-		import json
-		result = json.dumps(concordances,indent=4)+"\n"
-		output.write(result)
-
-	if args["format"] == "json":
-		output_as_json()
-	else:
-		output_as_xml()
-
-	output.close()
+if __name__=="__main__":
+	main()

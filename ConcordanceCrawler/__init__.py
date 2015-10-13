f89a898bfa2aa072f133e46099e7f51865fe2b7e
Dominik Macháček <gldkslfmsd@gmail.com>
2015-10-13 14:37:57 +0200

    modules split to new packages

--- ConcordanceCrawler/__init__.py
+++ ConcordanceCrawler/__init__.py
@@ -1,12 +1,1 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Dominik Macháček'
-__updated__ = '21.09.2015' # dd.mm.yyyy
-__version__ = '0.1.0'
-
-
-from ConcordanceCrawler.bazwords import *
-from ConcordanceCrawler.links import *
-from ConcordanceCrawler.visitor import *
-from ConcordanceCrawler.formatter import *
-from ConcordanceCrawler.logging_crawler import *
+'''tm is this module...'''

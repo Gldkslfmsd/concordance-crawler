3f0fb4fe4c30f70f169d28a7526cf489aebbe056
Dominik Macháček <gldkslfmsd@gmail.com>
2015-10-24 16:39:18 +0200

    improved setup.py for publishing on PyPI

--- setup.py
+++ setup.py
@@ -9,15 +9,33 @@ version = re.search(
 		 open('ConcordanceCrawler/__init__.py').read(),
 				    re.M).group(1)
 
+f = open("README.md","r")
+description = f.read()
+f.close()
+
 setup(name='ConcordanceCrawler',
       version=version,
       description='A module for automatic concordance extraction from the Internet',
-      long_description='TODO',
+      long_description=description,
       author='Dominik Macháček',
       author_email='gldkslfmsd@gmail.com',
-      url='https://github.com/Gldkslfmsd',
+      url='https://github.com/Gldkslfmsd/ConcordanceCrawler',
+			download_url = 'http://pypi.python.org/pypi/ConcordanceCrawler',
       py_modules=['usage'],
+			keywords='concordance, crawling, corpus, Internet',
+			license='TODO, some free licence',
+			platforms='any',
       packages=['ConcordanceCrawler'],
       entry_points={'console_scripts': ['ConcordanceCrawler	= ConcordanceCrawler.app:main']},
-      install_requires=['GoogleScraper','beautifulsoup4','dict2xml']
+      install_requires=['GoogleScraper','beautifulsoup4','dict2xml'],
+			test_suite='ConcordanceCrawler.tests',
+			classifiers = [
+				"Development Status :: 3 - Alpha",
+				"Environment :: Console",
+				"Intended Audience :: Science/Research",
+				"Operating System :: OS Independent",
+				# todo -- don't forget to fix it later
+				"Programming Language :: Python :: 3.4",
+				"Programming Language :: Python :: 3 :: Only",
+			],
 )

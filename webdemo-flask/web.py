cf7f5ec76d09866e44b105c576823fd47cc0b089
Dominik Macháček <gldkslfmsd@gmail.com>
2016-04-05 15:11:51 +0200

    flask webdemo: main page template

--- webdemo-flask/web.py
+++ webdemo-flask/web.py
@@ -13,7 +13,48 @@ main_page = """<html>
 </head>
 
 <body>
-něco
+
+<div id='header'>
+	<div id='header_content'>
+		<h1>ConcordanceCrawler</h1>
+<!--		<img src='static/logo.png' class='header-logo'>
+-->
+	</div>
+</div>
+
+
+<div id='content'>
+	<div id='left-column'>
+		<!-- Menu -->
+
+		<div class='box'>
+			<h2>Menu</h2>
+			<ul>
+				<li><a href='javascript:run_text("welcome");'>Home</a>
+				<li><a href='javascript:run_text("about");'>About</a>
+				<li><a href='javascript:run_submit()'>Submit new job</a>
+				<li><a href='javascript:run_list();'>Browse submitted jobs</a>
+				<li><a href='javascript:run_list();'>Download corpora</a>
+				<li><a href='javascript:run_text("contact");'>Contact</a>
+			</ul>
+		</div>
+
+
+		<div class='box' id='applet_server_status'>
+			<h2>Server status</h2>
+			<div class='loading'></div>
+			<div class='data'></div>
+		</div>
+	</div>
+
+	<div id='main-column'></div>
+	<div style='clear: both'></div>
+</div>
+
+<div id='footer'>
+	<p>2016 Dominik Macháček</p>
+</div>
+
 </body>
 </html>
 """
@@ -29,8 +70,6 @@ def images(image):
 @app.route("/javascript/<path:name>")
 def send_js(name):
 	return app.send_static_file("javascript/"+name)
-
-
 
 if __name__ == "__main__":
 	app.debug=True
